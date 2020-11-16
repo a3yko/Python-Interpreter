353505adbf338adc669fcf8d28636d2ecb8ade82
kaihualin <kl2cb@mail.missouri.edu>
2020-11-17 01:16:42 +0800

    added if/else and condition function

--- interp.py
+++ interp.py
@@ -33,11 +33,10 @@ def comment():
     
     chr+= 1
 
-
 # Assigns contents to variable
 def assign():
     global chr
-
+    
     varname = re.findall(r'^([a-zA-z0-9]+)\s+?=', src[chr:])[0]
     chr += len(varname)
     squeeze()
@@ -50,8 +49,7 @@ def assign():
         chr+= 1 
         while src[chr] != '\'':
             varcontent += src[chr]
-            chr+=1  
-                        
+            chr+=1            
         chr+=1
         variables[varname] = varcontent
         
@@ -62,24 +60,6 @@ def assign():
 
         #added eval so that it can add when assigned 
         variables[varname] = eval(varcontent)    
-    
-   
-   #lets you set a variable to another variable's value
-   #it doesn't let you do any operations on the values yet 
-    if src[chr] in '[a-zA-Z]':   
-        value = 0
-        while src[chr] != '\n':
-            varcontent += src[chr]
-            chr+=1
-
-            for x in variables:
-                if(varcontent == x):
-                    value += variables[varcontent]
-                    varcontent = ''
-
-        #added eval so that it can add when assigned 
-        variables[varname] = value 
-
     
     squeeze()
  
@@ -106,39 +86,87 @@ def printer():
         quit()
 
 
-
-
-#started the if condition 
-def ifcondition():
+def conditions():
     global chr
+
+    var1 = re.findall(r'^([a-zA-Z0-9]+)\s+?[==|!=|<|<=|>|>=]', src[chr:])[0]
+    condition = re.findall(r'(==|!=|>=|<=|>|<)', src[chr:])[0]
+    var2 = re.findall(r'[==|!=|<|<=|>|>=]\s+([a-zA-Z0-9]+)', src[chr:])[0]
+
+    chr += len(var1)
+    squeeze()
+    chr += len(condition)
+    squeeze()
+    chr += len(var2)
+    squeeze()
     
+
+    # make sure the varaible is in the list 
+    if (var1 in variables) and ((var2 in variables) or (var2.isdigit())):
+        if condition == "==":
+            return variables[var1] == variables[var2] if (var2 in variables) else int(var2)
+        if condition == "!=":
+            return variables[var1] != variables[var2] if (var2 in variables) else int(var2)
+        if condition == ">=":
+            return variables[var1] >= variables[var2] if (var2 in variables) else int(var2)
+        if condition == "<=":
+            return variables[var1] <= variables[var2] if (var2 in variables) else int(var2)
+        if condition == ">":
+            return variables[var1] > variables[var2] if (var2 in variables) else int(var2)
+        if condition == "<":
+            return variables[var1] < variables[var2] if (var2 in variables) else int(var2)
+
+    else:
+        print("this is not a condition")
+        quit()
+
+
+def if_else_block():
+    global chr
+
+    # skip if
     chr += 2
     squeeze()
-    chr += 1
-    condition = ''
-    
-    
-    while src[chr] != ')':
-        condition += src[chr]
-        chr += 1
 
-    
-    
+    condition = conditions()
+
+    if_statement = re.findall(r'^:\s+?([a-zA-Z0-9()"]+)\s+?else:', src[chr:])[0]
+
+    # skip colon
+    chr += 1
+
+    squeeze()
+    chr += len(if_statement)
+    squeeze()
+
+    # skip else
+    chr += 4
+    else_statement = re.findall(r'^:\s+([a-zA-Z0-9()\s"~!@`$%^&*_+-=:;]+)\s+?', src[chr:])[0]
+
+    #skip colon
     chr += 1
     squeeze()
-    if eval(condition):
-        print("true")
-        #needs a way to be able to read the lines following the if statement and ignore the else lines
-        
+    chr += len(else_statement)
+    squeeze()
+
+    if condition:
+        eval(if_statement)
     else:
-        print("untrue")
-        #same as if 
+        eval(else_statement)
 
 
 #Reader for input file
 while chr < len(src):
     if src[chr:].startswith('#'):
         comment()
+        continue
+
+    if re.match(r'^([a-zA-Z0-9]+)\s+?==|!=|<|<=|>|>=', src[chr:]):
+        print(conditions())
+        continue
+
+    if re.match(r'^if\s+?', src[chr:]):
+        if_else_block()
         continue
     
     if re.match(r'^([a-zA-z0-9]+)\s+?=', src[chr:]):
@@ -149,7 +177,4 @@ while chr < len(src):
         printer()
         continue
 
-    # if re.match(r'^if\s*\(', src[chr:]):
-    #     ifcondition()
-    #     continue
-
+    chr += 1
